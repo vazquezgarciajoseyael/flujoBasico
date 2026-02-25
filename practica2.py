6c317d58ab29a315329f3d5458d50231fe59fd6d
jose <joseyael1908@gmail.com>
2026-02-24 19:31:43 -0800

    practica2 lista

---
+++ practica2.py
@@ -0,0 +1,34 @@
+¿Qué es una Base De Datos Relacional?
+Una base de datos no relacional es aquella que guarda datos de un usuario ,los
+ puede guardar en archivos o otras formas como las tablas también organiza la información
+ y la categoría de esta.
+
+.¿Qué es una Tabla en el contexto Base De Datos Relacionales?
+Es aquella donde se categoriza la información en una tabla con filas y columnas.
+
+¿Qué es una relación en el contexto de Base De Datos Relacionales?
+Es como un tipo conexión entre la información y datos de diferentes tablas.
+
+¿Qué representa una columna en el contexto de Base De Datos Relacionales?
+. Una columna vendría siendo aquella que tiene lo más importante de los datos.
+
+¿Qué representa un renglón en el contexto de Base De Datos Relacionales?
+Pues yo digo que es como un tipo de clave o algo así.
+
+¿Qué es un motor de base de datos relacional?
+Es aquel que organiza todo asi como la información
+
+¿Cuáles son los motores de bases de datos relacionales más populares?
+Mysql también maria db ,sqlite y otros más.
+
+¿Cuál es el motor de base datos ligero que no necesita un servidor?
+Sqlite es ligero y el que es autónomo y no depende de un servidor
+para poder funcionar.
+
+¿Qué significa SQL en el contexto de base datos relacionales?
+tiene un significado de que es un lenguaje de consulta estructurada.
+
+¿Cuáles son los elementos de un sistema de información basado en base de datos relacionales?
+
+Un tipo de sistema que utiliza las tablas y que en esta se guardan datos y organiza dentrode
+sus filas y columnas.
